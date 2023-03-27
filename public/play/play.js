70bde8fd0acf58e9b998374426b75a67cbcbeef9
darthvader1925 <elijb12@gmail.com>
2023-03-26 20:14:33 -0700

    add enter and del handling

--- public/play/play.js
+++ public/play/play.js
@@ -4,11 +4,16 @@ var currentIndex = 0;
 document.querySelectorAll('.keyboard-button').forEach(btn => {
     btn.addEventListener('click', (e) => {
         const letter = btn.innerText.toLowerCase();
-        console.log(letter, "was pressed")
-        document.querySelector(`#row${currentRow}`).getElementsByClassName("tile")[currentIndex].innerText = letter;
 
-        if (currentIndex == 4) return; // change this later
-        currentIndex++;
+        if (letter == "enter") {
+
+        } else if (letter == "del") {
+
+        } else {
+            document.querySelector(`#row${currentRow}`).getElementsByClassName("tile")[currentIndex].innerText = letter;
+
+            if (currentIndex != 4) currentIndex++;
+        }
     })
 })
 
